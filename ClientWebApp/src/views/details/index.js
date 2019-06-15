2d14be6b65d1155ca3fbeeff87ceeda71ca65b5a
Mateusz Dobrakowski <mateusz.dobrakowski@avanade.com>
2019-06-15 15:16:35 +0200

    details styling part 1

--- ClientWebApp/src/views/details/index.js
+++ ClientWebApp/src/views/details/index.js
@@ -19,27 +19,28 @@ export default class Details extends React.Component {
         }
 
         return (
-            <article>
-                <section>
-                    <div>
-                        <img src="https://image.flaticon.com/icons/svg/194/194177.png" alt="zdj pieska" />
-                    </div>
-                    <div>
-                        <p>Lorem ipsum Maecenas tristique nisl at rhoncus commodo. Sed ac ligula ante. Cras a tristique metus. Suspendisse egestas sit amet mi quis pellentesque. Mauris commodo erat quis lectus placerat tristique. Etiam ornare ac elit id fermentum. Donec lacinia eros odio, nec lacinia lorem egestas vitae. Quisque viverra laoreet orci, in ultricies orci tempus eu. Pellentesque mollis lectus in vulputate tincidunt. Praesent non finibus metus. Curabitur elit nisi, sollicitudin sit amet libero et, maximus malesuada velit. Interdum et malesuada fames ac ante ipsum primis in faucibus. Praesent efficitur justo scelerisque, eleifend nunc sed, porttitor odio. Morbi aliquet a ante at aliquam. Curabitur imperdiet, lectus id scelerisque varius, nibh justo iaculis felis, id mollis lacus ipsum vitae nunc. Nunc tempus nibh at eros venenatis ullamcorper. In bibendum nulla vel arcu faucibus placerat. Maecenas tincidunt est purus. Nam nec mattis sem. In hac habitasse platea dictumst. Fusce accumsan dui maximus, luctus odio sed, vehicula mi. Vivamus vel neque purus. Nullam sodales nulla vitae augue luctus laoreet. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Praesent diam est, elementum a maximus a, porttitor in felis. Vivamus ut odio tristique, euismod enim bibendum, tincidunt libero. Nulla facilisi. Aenean vehicula nibh ac felis efficitur, in iaculis magna sollicitudin. Praesent ac libero interdum, efficitur elit ac, blandit lectus. Nulla facilisi. Pellentesque lobortis hendrerit dolor vel molestie. Mauris eu felis id ipsum elementum sagittis. Suspendisse tempus dapibus turpis id dictum. Quisque vehicula, quam sit amet euismod tristique, felis lorem gravida purus, quis sollicitudin justo magna at ipsum.</p>
-                    </div>
-                    <div>
-
-                        <div>
-                            <img src="/" />
-                            <p>asdasdasd</p>
+            <article className="article-details">
+                <section className="tile-details">
+                    <div className="wrap-tile">
+                        <div className="img">
+                            <img src="https://i.ytimg.com/vi/OdXSnjVCuzM/maxresdefault.jpg" alt="zdj pieska" />
                         </div>
-                        <div>
-                            <img src="/" />
-                            <p>asdasdasd</p>
+                        <div className="description">
+                            Lorem ipsum Maecenas tristique nisl at rhoncus commodo. Sed ac ligula ante. Cras a tristique metus. Suspendisse egestas sit amet mi quis pellentesque. Mauris commodo erat quis lectus placerat tristique. Etiam ornare ac elit id fermentum. Donec lacinia eros odio, nec lacinia lorem egestas vitae. Quisque viverra laoreet orci, in ultricies orci tempus eu. Pellentesque mollis lectus in vulputate tincidunt. Praesent non finibus metus. Curabitur elit nisi, sollicitudin sit amet libero et, maximus malesuada velit. Interdum et malesuada fames ac ante ipsum primis in faucibus. Praesent efficitur justo scelerisque, eleifend nunc sed, porttitor odio. Morbi aliquet a ante at aliquam. Curabitur imperdiet, lectus id scelerisque varius, nibh justo iaculis felis, id mollis lacus ipsum vitae nunc. Nunc tempus nibh at eros venenatis ullamcorper. In bibendum nulla vel arcu faucibus placerat. Maecenas tincidunt est purus. Nam nec mattis sem. In hac habitasse platea dictumst. Fusce accumsan dui maximus, luctus odio sed, vehicula mi. Vivamus vel neque purus. Nullam sodales nulla vitae augue luctus laoreet. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Praesent diam est, elementum a maximus a, porttitor in felis. Vivamus ut odio tristique, euismod enim bibendum, tincidunt libero. Nulla facilisi. Aenean vehicula nibh ac felis efficitur, in iaculis magna sollicitudin. Praesent ac libero interdum, efficitur elit ac, blandit lectus. Nulla facilisi. Pellentesque lobortis hendrerit dolor vel molestie. Mauris eu felis id ipsum elementum sagittis. Suspendisse tempus dapibus turpis id dictum. Quisque vehicula, quam sit amet euismod tristique, felis lorem gravida purus, quis sollicitudin justo magna at ipsum.
+                        </div>
+                    </div>
+                    <div className="buttons-tile">
+                        <div className="button-tile-item">
+                            <img src="/assets/leczenie.svg" alt="leczenie"/>
+                            <p className="txt"><div>Środki</div> na leczenie</p>
+                        </div>
+                        <div className="button-tile-item">
+                            <img src="/assets/naprawy.svg" alt="naprawy"/>
+                            <p className="txt"><div>Środki</div> na naprawy </p>
                         </div>
                     </div>
                 </section>
-                <section>
+                <section className="donate">
                     <div>
                         <h1>POMÓŻ</h1>
                     </div>
