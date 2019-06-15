4cdb4855f93eadf8e33a3670a467c9d08902f989
Marcin Motyka <marcin.motyka@avanade.com>
2019-06-15 10:31:54 +0200

    added  skeleton to sending email

---
+++ NotificationJobs/NotificationJobsLibrary/ISender.cs
@@ -0,0 +1,10 @@
+﻿using System.Net.Mail;
+using System.Threading.Tasks;
+
+namespace NotificationJobsLibrary
+{
+    public interface IMailSender
+    {
+        Task<EmailResult> SendMessageAsync(MailMessage mail);
+    }
+}
