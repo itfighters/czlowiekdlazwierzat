810ffa6e52eded8a64c4485e7720e766eb832066
Marcin Motyka <marcin.motyka@avanade.com>
2019-06-15 18:34:06 +0200

    added "subscription controller"

---
+++ WebApi/DAL/Repositories/Concrete/SubscriptionRepository.cs
@@ -0,0 +1,30 @@
+﻿using DAL.Model;
+using DAL.Repositories.Abstract;
+using Microsoft.EntityFrameworkCore;
+
+namespace DAL.Repositories.Concrete
+{
+    public class SubscriptionRepository : ISubscriptionRepository
+    {
+        private readonly DatabaseContext _dbContext;
+
+        public SubscriptionRepository(DatabaseContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public void AddSubscription(Subscription subscription)
+        {
+            try
+            {
+                _dbContext.Subscriptions.Add(subscription);
+                _dbContext.SaveChanges();
+
+            }
+            catch (DbUpdateException ex)
+            {
+                // Obsluga 
+            }
+        }
+    }
+}
