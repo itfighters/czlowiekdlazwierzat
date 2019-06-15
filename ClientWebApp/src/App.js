b275edb6a022e6cb66366f5815d71f077d0106b6
JakubCzechowski <kubaacze@gmail.com>
2019-06-15 18:03:25 +0200

    Remove loader from App.js

--- ClientWebApp/src/App.js
+++ ClientWebApp/src/App.js
@@ -3,7 +3,6 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 
 import Home from './views/home';
 import Notifications from './views/notifications';
-import Loader from './components/loader'
 import Header from './components/header';
 import Details from "./views/details";
 import Tiles from './views/tiles';
