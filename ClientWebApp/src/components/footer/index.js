edd5ed9507fdd7b60238d0b14f1a9e337b64d522
bsurzynski <bsurzynski.rcv@outlook.com>
2019-06-15 13:51:16 +0200

    footer add autopaly slider

--- ClientWebApp/src/components/footer/index.js
+++ ClientWebApp/src/components/footer/index.js
@@ -1,20 +1,13 @@
-//Footer
-//1. Dodaj sekcje z informacjami o partnerach, dodaj animacje
-//2. Dodaj sekcje z informacjami o fundacji
-//3. Dodaj działający przycisk z facebook
-
 import React, { Component } from 'react';
 import About from './about';
 import Partners from './partners'
-export default class Footer extends Component {
 
+export default class Footer extends Component {
     render() {
         return (
             <footer>
                 <Partners />
-
                 <About />
-
             </footer>
         )
     }
--- ClientWebApp/src/components/footer/partners.js
+++ ClientWebApp/src/components/footer/partners.js
@@ -2,27 +2,42 @@ import React, { Component } from 'react';
 import AliceCarousel from 'react-alice-carousel';
 import "react-alice-carousel/lib/alice-carousel.css";
 
-
-export default function Partners() {
-    var Testresponsive = {
-        0: { items: 2 },
-        1024: { items: 6 },
+export default class Partners extends Component {
+    constructor(props) {
+        super(props)
+        this.Carousel = React.createRef();
     }
 
+    render() {
+        var settings = {
+            mouseDragEnabled: true,
+            buttonsDisabled: true,
+            dotsDisabled: true,
+            autoPlayInterval: 3000,
+            autoPlay: true,
+            infinite: true,
+            responsive: {
+                0: { items: 2 },
+                1024: { items: 6 },
+            }
+        }
 
-    return (
-        <section>
-        <h1>
-            PARTNERZY
-        </h1>
-        <AliceCarousel mouseDragEnabled buttonsDisabled={true} dotsDisabled={true} autoPlayInterval={5000} infinite responsive={Testresponsive}  >
-            <a href="/" ><img alt="partner" src="https://itfighters.pl/wp-content/uploads/2018/08/home6-logo.png"></img></a>
-            <a href="/"><img alt="partner" src="https://itfighters.pl/wp-content/uploads/2018/08/home6-logo.png"></img></a>
-            <a href="/"><img alt="partner" src="https://itfighters.pl/wp-content/uploads/2018/08/home6-logo.png"></img></a>
-            <a href="/"><img alt="partner" src="https://itfighters.pl/wp-content/uploads/2018/08/home6-logo.png"></img></a>
-            <a href="/"><img alt="partner" src="https://itfighters.pl/wp-content/uploads/2018/08/home6-logo.png"></img></a>
-            <a href="/"><img alt="partner" src="https://itfighters.pl/wp-content/uploads/2018/08/home6-logo.png"></img></a>
-        </AliceCarousel>
-        </section>
-    )
+        return (
+            <section>
+                <h1>
+                    PARTNERZY
+                </h1>
+                <AliceCarousel {...settings} ref={(el) => (this.Carousel = el)}>
+                    <a href="/" ><img alt="partner" src="https://itfighters.pl/wp-content/uploads/2018/08/home6-logo.png"></img></a>
+                    <a href="/"><img alt="partner" src="https://itfighters.pl/wp-content/uploads/2018/08/home6-logo.png"></img></a>
+                    <a href="/"><img alt="partner" src="https://itfighters.pl/wp-content/uploads/2018/08/home6-logo.png"></img></a>
+                    <a href="/"><img alt="partner" src="https://itfighters.pl/wp-content/uploads/2018/08/home6-logo.png"></img></a>
+                    <a href="/"><img alt="partner" src="https://itfighters.pl/wp-content/uploads/2018/08/home6-logo.png"></img></a>
+                    <a href="/"><img alt="partner" src="https://itfighters.pl/wp-content/uploads/2018/08/home6-logo.png"></img></a>
+                </AliceCarousel>
+                <button onClick={() => this.Carousel._slidePrev()}>Prev button</button>
+                <button onClick={() => this.Carousel._slideNext()}>Next button</button>
+            </section>
+        )
+    }
 }
