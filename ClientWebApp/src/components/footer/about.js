0547ef3e2b486b035e6fe0b11cc5dfcadbb7ae1d
RobertNocon <nocon.robert@gmail.com>
2019-06-15 17:38:57 +0200

    Styles for footer

--- ClientWebApp/src/components/footer/about.js
+++ ClientWebApp/src/components/footer/about.js
@@ -4,25 +4,30 @@ export default class About extends Component {
     render() {
         return (
 
-                <section>
-                    <div>
-                        <b>Fundacja Człowiek Dla Zwierząt</b>
-                        <p>adres lalalallala</p>
+                <section className="bottom-footer">
+                    <div className="contact-container">
+                        <div>
+                            <p className="contact-name">Fundacja</p>
+                            <p className="contact-name">Człowiek Dla Zawierząt</p>
+                            <p>Łętkowice Kolonia 59</p>
+                            <p>32-107 Radziemice</p>
+                        </div>
+                        <div>
+                            <p>12378164811</p>
+                            <p>12378164811</p>
+                        </div>
+                        <div>
+                            <p>adres mailowy</p>
+                        </div>
+                        <div>
+                            <p>NIP</p>
+                        </div>
+                        <div className="facebook-icon">
+                            <a href="https://www.facebook.com/fundacjaczlowiekdlazwierzat" target="_blank">
+                            <img src="./assets/facebook-box.svg" alt="Logo-facebook" />
+                            </a>
+                        </div>
                     </div>
-                    <div>
-                        <p>12378164811</p>
-                        <p>12378164811</p>
-                    </div>
-                    <div>
-                        <p>adres mailowy</p>
-                    </div>
-                    <div>
-                        <p>NIP</p>
-                    </div>
-                    <div>
-                        <a href="https://www.facebook.com/fundacjaczlowiekdlazwierzat" target="_blank">Facebook</a>
-                    </div>
-
                 </section>
         )
     }
--- ClientWebApp/src/components/footer/partners.js
+++ ClientWebApp/src/components/footer/partners.js
@@ -9,34 +9,38 @@ export default class Partners extends Component {
     }
 
     render() {
-        var settings = {
-            mouseDragEnabled: true,
-            buttonsDisabled: true,
-            dotsDisabled: true,
-            autoPlayInterval: 3000,
-            autoPlay: true,
-            infinite: true,
-            responsive: {
-                0: { items: 2 },
-                1024: { items: 6 },
-            }
-        }
+        // var settings = {
+        //     mouseDragEnabled: true,
+        //     buttonsDisabled: true,
+        //     dotsDisabled: true,
+        //     autoPlayInterval: 3000,
+        //     autoPlay: true,
+        //     infinite: true,
+        //     responsive: {
+        //         0: { items: 2 },
+        //         1024: { items: 6 },
+        //     }
+        // }
 
         return (
-            <section>
-                <h1>
+            <section className="top-footer">
+                <h2>
                     PARTNERZY
-                </h1>
-                <AliceCarousel {...settings} ref={(el) => (this.Carousel = el)}>
-                    <a href="/" ><img alt="partner" src="https://itfighters.pl/wp-content/uploads/2018/08/home6-logo.png"></img></a>
-                    <a href="/"><img alt="partner" src="https://itfighters.pl/wp-content/uploads/2018/08/home6-logo.png"></img></a>
-                    <a href="/"><img alt="partner" src="https://itfighters.pl/wp-content/uploads/2018/08/home6-logo.png"></img></a>
-                    <a href="/"><img alt="partner" src="https://itfighters.pl/wp-content/uploads/2018/08/home6-logo.png"></img></a>
-                    <a href="/"><img alt="partner" src="https://itfighters.pl/wp-content/uploads/2018/08/home6-logo.png"></img></a>
-                    <a href="/"><img alt="partner" src="https://itfighters.pl/wp-content/uploads/2018/08/home6-logo.png"></img></a>
-                </AliceCarousel>
-                <button onClick={() => this.Carousel._slidePrev()}>Prev button</button>
-                <button onClick={() => this.Carousel._slideNext()}>Next button</button>
+                </h2>
+
+                <div className="partners">
+                    {/* <AliceCarousel {...settings} ref={(el) => (this.Carousel = el)}> */}
+                        <a href="/" ><img alt="partner" src="./assets/it_fighters.png"></img></a>
+                        <a href="/"><img alt="partner" src="./assets/avande.png"></img></a>
+                        <a href="/"><img alt="partner" src="./assets/play_sms.png"></img></a>
+                        <a href="/"><img alt="partner" src="./assets/pan_mi_kupi.png"></img></a>
+                        <a href="/"><img alt="partner" src="./assets/kancelaria.png"></img></a>
+                        <a href="/"><img alt="partner" src="./assets/milomi.png"></img></a>
+                    {/* </AliceCarousel> */}
+                    {/* <button onClick={() => this.Carousel._slidePrev()}>Prev button</button>
+                    <button onClick={() => this.Carousel._slideNext()}>Next button</button> */}
+
+                </div>
             </section>
         )
     }
