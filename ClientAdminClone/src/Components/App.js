cd0b30c0d4a2a33eeb0df9687b04b87a8cce5d55
olkafasolka <a.placek1@gmail.com>
2019-06-15 16:00:38 +0200

    Dodano formularze dla email, sms, push

--- ClientAdminClone/src/Components/App.js
+++ ClientAdminClone/src/Components/App.js
@@ -1,16 +1,24 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import { Admin } from './Admin';
 import { Login } from './Login';
 import { ProtectedRoutes } from './ProtectedRoutes'; 
 import '../Styles/App.css';
+import SmsBox from "./SmsBox";
+import EmailBox from "./EmailBox";
+import PushBox from "./PushBox"
+
 
 function App() {
   return (
     <Router>
       <div>
         <Route path="/login" component={Login} />
+        <Route path="/form" component={SmsBox} />
+        <Route path="/form2" component={EmailBox} />
+          <Route path="/form3" component={PushBox} />
         <ProtectedRoutes path="/admin" component={Admin}></ProtectedRoutes>
+          
       </div>
     </Router>
   );
