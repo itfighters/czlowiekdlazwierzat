7f91c5d1af94940e5598e7bfecaf5697af7669d5
Ewa Para <ewa.para@avanade.com>
2019-06-15 10:51:04 +0200

    Add notifications context

---
+++ NotificationJobs/NotificationJobsLibrary/Models/NotificationContext.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace NotificationJobsLibrary.Models
+{
+    class NotificationContext : DbContext
+    {
+        private readonly string _connection;
+        public NotificationContext(string connection) : base()
+        {
+            _connection = connection;
+        }
+
+        public NotificationContext(DbContextOptions<NotificationContext> options)
+            : base(options)
+        {
+        }
+
+        //public virtual DbSet<Application> Applications { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(_connection);
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //modelBuilder.Entity<Application>().ToTable("Applications", schemaName: "dto");
+        }
+    }
+}
