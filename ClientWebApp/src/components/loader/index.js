3de575ad3b4124df0562c3e43af2749a10c55ab5
RobertNocon <nocon.robert@gmail.com>
2019-06-15 14:34:14 +0200

    Styles for loader

--- ClientWebApp/src/components/loader/index.js
+++ ClientWebApp/src/components/loader/index.js
@@ -2,7 +2,6 @@ import React from 'react'
 
 export default function Loader({ visible }) {
     if (!visible) { return null; }
-    return <div>
-        loader
+    return <div className="loader">
     </div>
 }
