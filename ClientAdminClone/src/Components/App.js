e0cb7f7d8bf0c74f00a4b8530ac4b19a97212f48
nuaSiekier <wiktor.siekierka@gmail.com>
2019-06-15 12:13:59 +0200

    FormTest

--- ClientAdminClone/src/Components/App.js
+++ ClientAdminClone/src/Components/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import { Admin } from './Admin';
 import { Login } from './Login';
+import Form from './Form';
 import { ProtectedRoutes } from './ProtectedRoutes'; 
 import '../Styles/App.css';
 
@@ -11,6 +12,7 @@ function App() {
       <div>
         <Route path="/login" component={Login} />
         <ProtectedRoutes path="/admin" component={Admin}></ProtectedRoutes>
+        <Route path='/form' component={Form} />
       </div>
     </Router>
   );
