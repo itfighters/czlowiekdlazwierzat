14e56425a94c47d84fa005a9bd7ba58c140f2448
michal.stachecki <michal.stachecki@avanade.com>
2019-06-15 11:24:25 +0200

    Add foundation description.

---
+++ ClientWebApp/src/components/foundationDescription/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function FoundationDescription() {
+
+    return (
+      <section>
+        <h1>O FUNDACJI</h1>
+        <p>
+          Fundacja Człowiek Dla Zwierząt została założona przez Wiolettę Litwin-Mroczka, która kilkanaście lat temu postanowiła poświęcić swoje życie zwierzętom. W zakupionym przez nią gospodarstwie stworzyła miejsce, w którym schronienie znajdują konie, kozy, psy i koty. W łętkowickiej fundacji zwierzaki mają dobre warunki bytowania, odpowiednie wyżywienie, opiekę weterynaryjną, duże pastwiska i wybiegi, a przede wszystkim codzienną dawkę czułości i zainteresowania od ludzi, którzy się nimi zajmują.
+        </p>
+      </section>
+    );
+}
--- ClientWebApp/src/views/home/index.js
+++ ClientWebApp/src/views/home/index.js
@@ -1,7 +1,11 @@
 import React from 'react';
+import FoundationDescription from '../../components/foundationDescription';
 
 export default function Home() {
     return (
-        <h1>Home</h1>
+        <div>
+            <h1>Home</h1>
+            <FoundationDescription />
+        </div>
     )
 }
