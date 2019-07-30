ee26d6edc9f12d37b4869ee1e28f7b6e83f3db81
jakub.rozycki <jakub.rozycki@ITM2757>
2019-07-30 12:17:06 +0200

    build

--- ClientWebApp/src/config.js
+++ ClientWebApp/src/config.js
@@ -1,1 +1,1 @@
-export const API_URL = "https://localhost:44335/api/";
+export const API_URL = "https://137.74.170.194/api/";
--- WebApi/NotificationJobs/NotificationHelper.cs
+++ WebApi/NotificationJobs/NotificationHelper.cs
@@ -4,9 +4,7 @@ using Infrastructure;
 using Microsoft.Extensions.Options;
 using NotificationJobs.Services.Abstract;
 using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Logging;
 
