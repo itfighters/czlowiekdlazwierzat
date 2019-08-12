7e3a7d8276ca8340821206bfe58a0fa4a5ba587d
jakub.rozycki <jakub.rozycki@ITM2757>
2019-08-12 11:39:53 +0200

    fix

--- WebApi/NotificationJobsLibrary/Services/Concrete/EmailService.cs
+++ WebApi/NotificationJobsLibrary/Services/Concrete/EmailService.cs
@@ -44,7 +44,7 @@ namespace NotificationJobsLibrary.Services.Concrete
 
         public async Task<SendNotificationResult> SendMessage(string email, string subject, string body)
         {
-            var message = MailMessageCreator.GenerateMessage(email, options.Value.From, subject, body, Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "Templates\\Resources"));
+            var message = MailMessageCreator.GenerateMessage(email, options.Value.From, subject, body, Path.Combine(Directory.GetCurrentDirectory(), "Templates", "Resources"));
             return await SendMessage(message);
         }
     }
