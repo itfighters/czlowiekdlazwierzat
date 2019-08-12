6f2964cf18bef8dc8ac89f58ae71c760f8708705
jakub.rozycki <jakub.rozycki@ITM2757>
2019-08-12 09:55:09 +0200

    changes

--- ClientWebApp/src/config.js
+++ ClientWebApp/src/config.js
@@ -1,5 +1,5 @@
-export const API_URL = "https://localhost:44335/api/";
-export const IMAGES_URL = "https://localhost:44335/images";
+export const API_URL = "https://pomagalnia.pl/api/";
+export const IMAGES_URL = "https://pomagalnia.pl/images";
 
 export const PlaceholderImg =
   "http://www.workingthedoors.co.uk/wp-content/themes/petsitter/images/job-placeholder.gif";
