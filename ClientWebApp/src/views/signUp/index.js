b086830b73fd9bb5f488523cd57764e92b869a3a
bsurzynski <surzynskidev@gmail.com>
2019-08-16 22:47:01 +0200

    fix warning

--- ClientWebApp/src/views/signUp/index.js
+++ ClientWebApp/src/views/signUp/index.js
@@ -283,7 +283,7 @@ export default class SignUp extends Component {
       }
       return token;
     } catch (error) {
-      if (error.code == "messaging/unsupported-browser") {
+      if (error.code === "messaging/unsupported-browser") {
         this.showToast(
           "Twoja przeglądarka nie wspier push powiadomień",
           "warning"
