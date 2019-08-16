4af7838fa5149284a0b758a6f919eb646c49e264
Maciej Rozycki <m.rozycki@avanade.com>
2019-08-16 23:27:59 +0200

    Email notifications

--- WebApi/NotificationJobsLibrary/Services/Concrete/EmailService.cs
+++ WebApi/NotificationJobsLibrary/Services/Concrete/EmailService.cs
@@ -34,17 +34,17 @@ namespace NotificationJobsLibrary.Services.Concrete
                 client.DeliveryMethod = SmtpDeliveryMethod.Network;
                 client.Credentials = new NetworkCredential(options.Value.UserName, options.Value.Password);
                 client.Send(mail);
-                return Task.FromResult(new SendNotificationResult());
+                return Task.FromResult(new SendNotificationResult(){IsSuccessful = true});
             }
             catch (Exception exp)
             {
-                return Task.FromResult(new SendNotificationResult());
+                return Task.FromResult(new SendNotificationResult(){IsSuccessful = false, LogMessage = exp.Message});
             }
         }
 
         public async Task<SendNotificationResult> SendMessage(string email, string subject, string body)
         {
-            var message = MailMessageCreator.GenerateMessage(email, options.Value.From, subject, body, Path.Combine(Directory.GetCurrentDirectory(), "Templates", "Resources"));
+            var message = MailMessageCreator.GenerateMessage(email, options.Value.From, subject, body, Path.Combine(Path.GetDirectoryName(Assembly.GetEntryAssembly().Location), "Templates", "Resources"));
             return await SendMessage(message);
         }
     }
