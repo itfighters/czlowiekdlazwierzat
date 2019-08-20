19775f71af88339c2bf0ec4351215798fe1e71b3
jakub.rozycki <jakub.rozycki@ITM2757>
2019-08-20 10:55:58 +0200

    small changes

--- ClientWebApp/src/views/details/index.js
+++ ClientWebApp/src/views/details/index.js
@@ -122,7 +122,7 @@ export default class Details extends React.Component {
                   <form
                     action="https://www.paypal.com/cgi-bin/webscr"
                     method="post"
-                    target="_top"
+                    target="_blank"
                   >
                     <input type="hidden" name="cmd" value="_donations" />
                     <input
